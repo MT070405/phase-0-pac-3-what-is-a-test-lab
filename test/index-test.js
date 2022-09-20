970ea317e0ebf1a5c154b5d626d1694893cb061c
Lukas <MT910720@yahoo.com>
2022-09-20 01:58:05 -0400

    commit tree 802653eee44378e12b4efaac661b2f8459fbfb1d

--- test/index-test.js
+++ test/index-test.js
@@ -3,12 +3,12 @@ const { name, height, message } = require("../index.js");
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      expect(name). toEqual ("Susan");
     });
   });
 
   describe("Height", () => {
-    it("is less than 40", () => {
+    it("is less than 39", () => {
       expect(height).toBeLessThan(40);
     });
   });
